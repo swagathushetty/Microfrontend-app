e67dea1ba17e6a5acf121538a343237d255a9b4e
swagath shetty <swagath.shetty@gmail.com>
2022-06-29 21:46:59 +0530

    init commit

---
+++ pdp/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Header from "home/Header";
+import Footer from "home/Footer";
+import "./index.scss";
+
+const App = () => (
+  <div className="text-3xl mx-auto max-w-6xl">
+     <Header />
+        <div className="my-10">
+          Home page content
+        </div>
+     <Footer />
+  </div>
+);
+ReactDOM.render(<App />, document.getElementById("app"));
